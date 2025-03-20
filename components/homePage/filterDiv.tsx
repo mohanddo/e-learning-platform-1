515e4bc6a0c99c3b974293ee734cc24d56851f17
Youcef <abdelhakim1cef@gmail.com>
2025-03-19 17:20:48 -0700

    first commit

---
+++ components/homePage/filterDiv.tsx
@@ -0,0 +1,25 @@
+import { Search } from "lucide-react";
+const FilterDiv = () => {
+
+    return (
+        <div className="flex flex-row h-[10vh]  items-center">
+            <div className="flex-1 flex justify-start pl-32">
+                <p className="text-3xl font-bold flex pl-16 ">
+                    Our <span className="text-[var(--addi-color-500)]">Courses</span>
+                </p>
+            </div>
+            <div className="flex-1 flex items-center justify-center">
+                <div className="border-1 border-solid border-gray-400 flex flex-row items-center px-1 py-1 w-auto rounded-lg">
+                    <input type="text"
+                        placeholder="Seartch for a course" 
+                        className="focus:outline-none focus:ring-0 focus:border-transparent w-md"/>
+                    <button className="px-1.5 rounded-lg py-1.5 bg-[var(--color-100)] text-[var(--color-400)] hover:text-[var(--addi-color-500)]">
+                        <Search />
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default FilterDiv;
---
+++ components/homePage/firstSection.tsx
@@ -0,0 +1,39 @@
+import { Section } from "lucide-react";
+import { exportTraceState } from "next/dist/trace"
+import { Button } from "@/components/ui/button"
+
+
+
+const FirstSection = () => {
+
+    return (
+        <section className="w-full h-auto flex flex-row pt-28 bg-[var(--color-50)]">
+            <div className="flex-1 flex flex-col pt-36 pl-16">
+                <p className="text-[var(--addi-color-500)] text-sm font-bold mb-9">
+                    Start Your favorit cours
+                </p>
+                <div className="w-[50%] mb-16">
+                    <p className="text-4xl font-bold">
+                        Now learning from anywhere, and build your <span className="text-[var(--addi-color-500)]">bright career</span>
+                    </p>
+                </div>
+                <div className="w-[50%] mb-9">
+                    <p className="text-sm font-bold text-gray-400">
+                        it has survived not only five centuries but also the leap into electronic typesetting
+                    </p>
+                </div>
+                <div className="w-full">
+                    <Button variant="outline" className="text-white text-lg font-bold bg-[var(--addi-color-500)] px-10 py-6">
+                        start course
+                    </Button>
+                </div>
+            </div>
+            <div className="flex-1 flex items-center justify-center">
+                <img src="/image-1-el.png" alt="image non disponible"
+                    className="w-[70%]" />
+            </div>
+        </section>
+    )
+}
+
+export default FirstSection;
