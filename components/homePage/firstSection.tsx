eb96caa3520c2c2bab0dfec3be8e1951e1582490
youcef <abdelhakim1cef@gmail.com>
2025-03-20 15:14:01 +0100

    .

--- components/homePage/firstSection.tsx
+++ components/homePage/firstSection.tsx
@@ -6,8 +6,8 @@ import { Button } from "@/components/ui/button"
 const FirstSection = () => {
 
     return (
-        <section className="h-screen w-full flex flex-row pt-28 bg-[var(--color-50)]">
-            <div className="flex-1 flex flex-col pt-[10%] pl-16">
+        <section className="h-auto w-full flex flex-row pt-[3%] bg-[var(--color-50)]">
+            <div className="flex-1 flex flex-col h-screen pt-[10%] pl-16">
                 <p className="text-[var(--addi-color-500)] text-sm font-bold mb-9">
                     Start Your favorit cours
                 </p>
@@ -16,7 +16,7 @@ const FirstSection = () => {
                         Now learning from anywhere, and build your <span className="text-[var(--addi-color-500)]">bright career</span>
                     </p>
                 </div>
-                <div className="w-[50%] mb-9">
+                <div className="w-[70%] mb-9">
                     <p className="text-sm font-bold text-gray-400">
                         it has survived not only five centuries but also the leap into electronic typesetting
                     </p>
