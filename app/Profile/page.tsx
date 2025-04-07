da4fefa81d999181f579efd7efb813dfcb06abc9
Youcef <abdelhakim1cef@gmail.com>
2025-04-07 05:11:14 -0700

    working on profile

--- app/Profile/page.tsx
+++ app/Profile/page.tsx
@@ -2,11 +2,15 @@
 
 const Profile = () => {
 
-    return(
-        <div>
-            youcef
-            
-        </div>
+    return (
+        <section className="bg-amber-300 flex justify-center h-[80vh] pt-[13vh]">
+            <section className="bg-green-200 w-[90%]  flex flex-col rounded-lg">
+                <div>
+                    <span className="px-5 py-2 rounded-xl bg-blue-300 text-blue-500 border border-blue-500">Student</span>
+                    <p className="text-3xl font-bold mt-5">My Profile</p>
+                </div>
+            </section>
+        </section>
     )
 }
 
